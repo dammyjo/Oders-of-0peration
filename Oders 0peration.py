e41cf1748ce9838d18176ad905c47ec570769524
dammyjo <112008172+dammyjo@users.noreply.github.com>
2022-09-06 13:17:59 +0100

    Add files via upload

---
+++ Oders 0peration.py
@@ -0,0 +1,18 @@
+height = int(input("What is the height of the triangle? "))
+base = int(input("What is the base of the triangle? "))
+area = base * height /2
+
+print("The area of the triangle would be {0:f} ".format(area))
+
+
+
+breadth = float(input(" What is the height of the rectanngle? : "))
+length = float(input("What is the height of the rectangle? : "))
+area = breadth* length
+print("The area is ",breadth* length)
+
+
+
+lenght = int(input("Enter the length of the square"))
+length= int(input("Enter the length of the square"))
+print("The area of the square is",length*length)
